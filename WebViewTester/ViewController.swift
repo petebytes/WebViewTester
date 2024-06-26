1a9d699ecccde8ce432dfed06fef33fed5209c69
petebytes <pete@apsion.com>
2024-06-25 20:44:24 -0700

    base

---
+++ WebViewTester/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WebViewTester
+//
+//  Created by Ghar on 6/25/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
